a365528c75eab6db17dccd2032a70923454719aa
cristofervidal <cristofervidal26@gmaial.com>
2018-12-13 21:54:42 -0500

    TABLACOMPRAS
    carrrito de compras en la BD

--- registro.sql
+++ registro.sql
@@ -1,4 +1,4 @@
--- phpMyAdmin SQL Dump
+﻿-- phpMyAdmin SQL Dump
 -- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
@@ -62,3 +62,36 @@ ALTER TABLE `persona`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+
+create table productos(
+	id_producto int primary key auto_increment,
+	nombre varchar(255) not null,
+	img_producto varchar(255) not null,
+	id_categoria int not null,
+	precio float not null,
+	stock int not null
+);
+
+DELIMITER //
+	CREATE PROCEDURE selectProductos()
+	BEGIN
+		SELECT * FROM productos;
+	END	
+//
+
+DELIMITER //
+	CREATE PROCEDURE selectProducto(
+		IN _id_producto int
+	)
+	BEGIN
+		SELECT * FROM productos WHERE id_producto = _id_producto;
+	END	
+//
+
+insert into productos(nombre, img_producto, id_categoria, precio, stock) values('Camisa Manga Larga', 'images/home/gallery2.jpg', 2, 39.99, 10); 
+insert into productos(nombre, img_producto, id_categoria, precio, stock) values('Vestido negro', 'images/home/product3.jpg', 2, 19.99, 20);
+insert into productos(nombre, img_producto, id_categoria, precio, stock) values('Sueter azul', 'images/home/product5.jpg', 2, 29.99, 20);
+insert into productos(nombre, img_producto, id_categoria, precio, stock) values('Camiseta blanca', 'images/home/product6.jpg', 2, 13.99, 15);
