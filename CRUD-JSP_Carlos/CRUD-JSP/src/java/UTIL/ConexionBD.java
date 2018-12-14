cf0a6500fb8bc459ca3c4bad3da4b6d6efa100b1
IqueMart <martinezch.carlos@gmail.com>
2018-12-13 21:59:51 -0500

    CRUD-JSP_Carlos

---
+++ CRUD-JSP_Carlos/CRUD-JSP/src/java/UTIL/ConexionBD.java
@@ -0,0 +1,27 @@
+
+package UTIL;
+
+import java.sql.Connection;
+import java.sql.*;
+
+public class ConexionBD {
+     private static Connection cnx = null;
+   public Connection obtener() throws SQLException, ClassNotFoundException {
+      if (cnx == null) {
+         try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cnx = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_practica", "root", "");
+         } catch (SQLException ex) {
+            throw new SQLException(ex);
+         } catch (ClassNotFoundException ex) {
+            throw new ClassCastException(ex.getMessage());
+         }
+      }
+      return cnx;
+   }
+   public static void cerrar() throws SQLException {
+      if (cnx != null) {
+         cnx.close();
+      }
+   }
+}
